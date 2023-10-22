7ff840c99725c25666b8689ae18c7d3bc0c66520
安小歪 <119773750+shangskr@users.noreply.github.com>
2023-10-22 16:01:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  URL: process.env.MODB_URL || 'mongodb://127.0.0.1:27017/paste',
+  URL: process.env.MODB_URL || 'mongodb+srv://anxiaowai:anxiaowai123@cluster0.dwgdwrg.mongodb.net/?retryWrites=true&w=majority',
   // 加密字符串
   session_secret: 'chuckle',
   token_secret: 'chuckle'
